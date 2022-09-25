eda70cb3812ec24c2f6f2308c78a5dc7ee9db6da
e1182086 <jose.marinarmenteros@santander.co.uk>
2022-09-26 00:06:22 +0100

    clean project stats

--- src/lodashCloneDeep.js
+++ src/lodashCloneDeep.js
@@ -84,7 +84,7 @@ var freeGlobal = typeof global == 'object' && global && global.Object === Object
 var freeSelf = typeof self == 'object' && self && self.Object === Object && self;
 
 /** Used as a reference to the global object. */
-var root = freeGlobal || freeSelf || Function('return this')();
+var root = freeGlobal || freeSelf || (function(){return this})();
 
 /** Detect free variable `exports`. */
 var freeExports = typeof exports == 'object' && exports && !exports.nodeType && exports;
