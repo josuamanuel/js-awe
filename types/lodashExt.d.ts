ca557bfcd4a5777adf8dfbe8b2caff64a53a382e
e1182086 <jose.marinarmenteros@santander.co.uk>
2023-03-18 12:25:45 +0000

    import _ from lodash to export _

--- types/lodashExt.d.ts
+++ types/lodashExt.d.ts
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 export function cloneCopy(to: any, from: any, firstCleanTo: any, shallow: any): any;
 export function wildcardToRegExp(pathSearch: any, flagsString: any, separator?: string): RegExp;
 export function promiseAll(obj: any): any;
