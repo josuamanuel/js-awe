4580d737a00281cda0817e9d48a60e03699417dd
josuamanuel <josuamanuel@hotmail.com>
2023-06-04 20:53:34 +0100

    remove streamUtils

--- types/streamUtils.d.ts
+++
@@ -1,6 +0,0 @@
-export const pipeline: any;
-export function mapStream(mapFunc: any): import("stream-transform").Transformer;
-export function filterStream(filterFunc: any): import("stream-transform").Transformer;
-export function filterMapStream(filterFunc: any, mapFunc: any): import("stream-transform").Transformer;
-export const fsWriteFilePromise: any;
-//# sourceMappingURL=streamUtils.d.ts.map
