90dba62754f93403ca07f47ce112aea4691ec567
josuamanuel <josuamanuel@hotmail.com>
2023-09-15 08:38:28 +0100

    fix build

--- awkChangenNodePerf.sh
+++ awkChangenNodePerf.sh
@@ -2,7 +2,7 @@ awk 'BEGIN{
 print "let myGlobal = typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : typeof this !== 'undefined' ? this : {}";
 print "let performance = myGlobal.performance"
 print "if(performance === undefined) performance = {}"}
-     {if(NR>1)print}' ./dist/browser/js-awe.js > ./dist/browser/js-awe2.js;
+     {if(NR>0)print}' ./dist/browser/js-awe.js > ./dist/browser/js-awe2.js;
 
 rm ./dist/browser/js-awe.js
 mv ./dist/browser/js-awe2.js ./dist/browser/js-awe.js
