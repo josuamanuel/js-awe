82bed437cc204476c7d9c0a1b2f858359aa901c5
Jose Marin Armenteros <jose.marinarmenteros@santander.co.uk>
2025-03-14 14:19:33 +0000

    Add fetchBulkCurrentAccounts function and integrate into customer balance retrieval

--- examples/nodeImport/plan/complexPlan.js
+++ examples/nodeImport/plan/complexPlan.js
@@ -2,14 +2,27 @@ import { plan } from 'js-awe'
 
 const { build, map } = plan()
 const getCustomerBalances = build([
-  fetchAccounts,
-  [l => l],
-  [filterSavings, pluck('id'), map(fetchSavingBalance)],
-  [filterLoans, pluck('id'), map(fetchLoanBalance)],
+  [fetchBulkCurrentAccounts],
+  [fetchAccounts,
+    [l => l],
+    [filterSavings, pluck('id'), map(fetchSavingBalance)],
+    [filterLoans, pluck('id'), map(fetchLoanBalance)],
+  ],
   format,
 ])
 
 console.log('result: ', await getCustomerBalances('0396d9b0'))
+
+function fetchBulkCurrentAccounts() {
+  return Promise.resolve(
+    [
+      {
+        id: 13,
+        balance: 9
+      },
+    ]
+  )
+}
 
 function filterSavings(accounts) {
   return accounts.filter((account) => account.type === 'saving')
@@ -19,12 +32,13 @@ function filterLoans(accounts) {
   return accounts.filter((account) => account.type === 'loan')
 }
 
-function format([accounts, savingBalances, loanBalances]) {
+function format([fetchBulkCurrentAccounts, [accounts, savingBalances, loanBalances]]) {
 
   return accounts.map(account => {
     let balance
     if(account.type === 'saving') balance = savingBalances.find(savingAccount => savingAccount.id === account.id ).balance
-    else  balance = loanBalances.find(loanAccount => loanAccount.id === account.id ).balance
+    if(account.type === 'loans') balance = loanBalances.find(loanAccount => loanAccount.id === account.id ).balance
+    if(account.type === 'current') balance = fetchBulkCurrentAccounts.find(contract => contract.id === account.id ).balance
 
     return {...account, balance}
   })
@@ -43,6 +57,7 @@ function fetchAccounts(customerId) {
   return Promise.resolve([
     { id: 1, type: 'saving' },
     { id: 2, type: 'loan' },
+    { id: 13, type: 'current' },
   ])
 }
 
